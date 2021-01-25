36c968182448aaafd568ac9eaa35c603a842cfa3
Nikolai Nekrutenko <niknek@pop-os.localdomain>
2021-01-25 17:23:07 -0500

    organized ultimatetestcode

--- ultimatetest.py
+++ ultimatetest.py
@@ -1,5 +1,5 @@
 # Import packages
-from time import sleep
+import sleep
 import board
 import busio
 import adafruit_lsm303_accel
@@ -8,8 +8,12 @@ import adafruit_bmp3xx
 
 # Connect to the sensors
 i2c=busio.I2C(board.SCL, board.SDA)
+
+# Mag and accel sensor
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
+
+# Temp and pressure sensor
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 bmp.pressure_oversampling = 8
 bmp.temperature_oversampling = 2
