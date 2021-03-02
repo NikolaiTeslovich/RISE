c69a0008d492232d5e3d7eee2ccb402a17874db1
Nikolai Nekrutenko <nekrutnikolai@pop-os.localdomain>
2021-03-02 17:39:59 -0500

    worked on code

--- pandas_demo_3.py
+++ pandas_demo_3.py
@@ -1,24 +1,25 @@
-import pandas 
-import board 
-import busio 
-import adafruit_bmp3xx 
+import pandas
+import board
+import busio
+import adafruit_bmp3xx
 import adafruit_lsm303_accel
 import adafruit_lsm303dlh_mag
-##### let's add in some extra functionality #####
 import time #time package (if you want to use the sleep command and/or keep track of time)
 import picamera #running the spycamera while gathering datas
 
 ##### set up I2C and sensors #####
-i2c = busio.I2C(board.SCL,board.SDA) 
-bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c) 
+i2c = busio.I2C(board.SCL,board.SDA)
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
 ##### let's say we want to make a list of all sensor measurements and alo keep track of time... #####
 df = pandas.DataFrame(columns=['time','temperature','pressure',
 								'acc_x','acc_y','acc_z',
-								'mag_x','mag_y','mag_z']) 
+								'mag_x','mag_y','mag_z'])
 #set up a DataFrame with columns time, for temp, pressure, 3 columns for acceleraton and 3 for magnetometer
+
+# CHECK OTHER CAMERA FUNCTIONS
 
 start_time = time.time() #reference for t=0
 cam = picamera.PiCamera() #intiialize camera
@@ -30,14 +31,15 @@ for i in range(10): #repeat measurements and data append 10 times (perahps incre
 	df = df.append({'time':elapsed_time, #add in current time
 					'temperature':bmp.temperature,
 					'pressure':bmp.pressure,
-					'acc_x':accel.acceleration[0],
-					'acc_y':accel.acceleration[1],
-					'acc_z':accel.acceleration[2],
-					'mag_x':mag.magnetic[0],
-					'mag_y':mag.magnetic[1],
-					'mag_z':mag.magnetic[2]},ignore_index=True)
+					'accx':accel.acceleration[0],
+					'accy':accel.acceleration[1],
+					'accz':accel.acceleration[2],
+					'magx':mag.magnetic[0],
+					'magy':mag.magnetic[1],
+					'magz':mag.magnetic[2]},ignore_index=True)
 	time.sleep(1) #optional: specify rate of data collection
+
 cam.stop_recording()
-df.to_csv('time_temp_press_accel_mag.csv') #to save data as CSV file
+df.to_csv('data.csv') #to save data as CSV file
 total_elapsed = time.time() - start_time
 print(total_elapsed)
