a3019c4102b13119511f12773f9f17c56ea88b32
Nikolai Nekrutenko <niknek@nikolais-mbp.lan>
2021-03-08 15:44:36 -0500

    working on program

--- theprogram.py
+++ theprogram.py
@@ -4,7 +4,7 @@ import busio
 import adafruit_bmp3xx
 import adafruit_lsm303_accel
 import adafruit_lsm303dlh_mag
-import time #time package (if you want to use the sleep command and/or keep track of time)
+import time #time package (if you wa©nt to use the sleep command and/or keep track of time)
 import picamera #running the spycamera while gathering datas
 
 ##### set up I2C and sensors #####
@@ -14,17 +14,18 @@ accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
 ##### let's say we want to make a list of all sensor measurements and alo keep track of time... #####
+#set up a DataFrame with columns time, for temp, pressure, 3 columns for acceleraton and 3 for magnetometer
 df = pandas.DataFrame(columns=['time','temperature','pressure',
 								'acc_x','acc_y','acc_z',
 								'mag_x','mag_y','mag_z'])
-#set up a DataFrame with columns time, for temp, pressure, 3 columns for acceleraton and 3 for magnetometer
 
-# CHECK OTHER CAMERA FUNCTIONS
+# CHECK OTHER CAMERA FUNCTIONS https://picamera.readthedocs.io/en/release-1.12/fov.html
 
 start_time = time.time() #reference for t=0
-cam = picamera.PiCamera() #intiialize camera
-cam.resolution = (640,800) #set camera resolution
-cam.start_recording('testvid.h264') #start recording and name video file
+camera = picamera.PiCamera() #intiialize camera
+camera.resolution = (1296, 972) #set camera resolution
+camera.framerate = 30
+camera.start_recording('testvid.h264') #start recording and name video file
 
 for i in range(10): #repeat measurements and data append 10 times (perahps increase to 100 or 1000)
 	elapsed_time = time.time() - start_time # current time in seconds since start_time
@@ -39,7 +40,7 @@ for i in range(10): #repeat measurements and data append 10 times (perahps incre
 					'magz':mag.magnetic[2]},ignore_index=True)
 	time.sleep(1) #optional: specify rate of data collection
 
-cam.stop_recording()
+camera.stop_recording()
 df.to_csv('data.csv') #to save data as CSV file
 total_elapsed = time.time() - start_time
 print(total_elapsed)
