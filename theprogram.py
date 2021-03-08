fca7075a843991b233f31a9f74ef764eaeb8c8e8
Nikolai Nekrutenko <niknek@nikolais-mbp.lan>
2021-03-08 18:39:03 -0500

    working on program

--- theprogram.py
+++ theprogram.py
@@ -1,3 +1,5 @@
+Flight time is estimated to be three minutes
+
 import pandas
 import board
 import busio
@@ -21,15 +23,19 @@ df = pandas.DataFrame(columns=['time','temperature','pressure',
 
 # CHECK OTHER CAMERA FUNCTIONS https://picamera.readthedocs.io/en/release-1.12/fov.html
 
-start_time = time.time() #reference for t=0
+## Compare pressure sensor data to make it work
+
+start_time = time.time() #reference for t = 0
 camera = picamera.PiCamera() #intiialize camera
-camera.resolution = (1296, 972) #set camera resolution
+camera.resolution = (1296, 972) #set camera resolution, this one utilized the whole sensor while keeping a decent framerate
 camera.framerate = 30
 camera.start_recording('testvid.h264') #start recording and name video file
 
-for i in range(10): #repeat measurements and data append 10 times (perahps increase to 100 or 1000)
-	elapsed_time = time.time() - start_time # current time in seconds since start_time
-	df = df.append({'time':elapsed_time, #add in current time
+while True:
+
+	for i in range(100): #repeat measurements and data append 10 times (perahps increase to 100 or 1000)
+		elapsed_time = time.time() - start_time # current time in seconds since start_time
+		df = df.append({'time':elapsed_time, #add in current time
 					'temperature':bmp.temperature,
 					'pressure':bmp.pressure,
 					'accx':accel.acceleration[0],
@@ -37,10 +43,12 @@ for i in range(10): #repeat measurements and data append 10 times (perahps incre
 					'accz':accel.acceleration[2],
 					'magx':mag.magnetic[0],
 					'magy':mag.magnetic[1],
-					'magz':mag.magnetic[2]},ignore_index=True)
-	time.sleep(1) #optional: specify rate of data collection
+					'magz':mag.magnetic[2]}, ignore_index=True)
+
+	df.to_csv('data.csv') #to save data as CSV file
+
+
 
 camera.stop_recording()
-df.to_csv('data.csv') #to save data as CSV file
 total_elapsed = time.time() - start_time
 print(total_elapsed)
