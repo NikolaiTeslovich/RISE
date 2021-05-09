8d78da26c8ef1aaed5872ffb629698e3f240cbc2
NikolaiTeslovich <nekrutnikolai@gmail.com>
2021-05-09 14:05:08 -0400

    renamed files

--- flightcode.py
+++ flightcode.py
@@ -1,5 +1,5 @@
 import os
-import pandas
+import numpy as np
 import board
 import busio
 import adafruit_bmp3xx
@@ -8,40 +8,48 @@ import adafruit_lsm303dlh_mag
 import time
 import picamera
 
-# Delete the csvs if they already exist to eliminate any confusion
-for file_name in os.listdir('/home/pi/RISE/data'):
-    if file_name.endswith('.csv'):
-        os.remove(f'/home/pi/RISE/data/{file_name}')
+# Setup a directory to save the data in
+
+main_dir = os.listdir('/home/pi/RISE')
+
+names = np.array([0])
+
+for name in main_dir:
+    if name.startswith('data'):
+        name = np.array([name[-1]]).astype(np.int)
+        names = np.append(names, name, axis=0).astype(np.int)
+
+data_dir = f'/home/pi/RISE/data{np.max(names) + 1}'
+
+os.mkdir(data_dir)
+
+print('made directory at ' + data_dir)
 
 # Set up I2C and sensors
+
 i2c = busio.I2C(board.SCL, board.SDA)
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
-# DataFrame with columns time, temp, pressure, 3 for acceleraton and 3 for magnetometer
-df = pandas.DataFrame(columns=['time','temperature','pressure',
-                               'acc_x','acc_y','acc_z',
-                               'mag_x','mag_y','mag_z'])
+# Define the time parameters
 
-# Set up camera stuff.
-### MAYBE ALSO ITERATE THE CAMERA RECORDINGS ###
-# cam = picamera.PiCamera() #intialize camera
-# cam.resolution = (640, 480)
-# camera.framerate = 90
-# cam.start_recording('video.h264') #start recording and name video file
-
-# Define start time, delay, and the record time
 start_time = time.time()
-delay_time = 5
-record_time = 20
+delay_time = 10
+record_time = 300
 
 # iterating parameters
-i = 0
-iters = 20
 
-# FOR TESTING
-print('Start')
+i = 0
+iters = 500
+
+elapsed_time = time.time() - start_time
+
+# Define the array with all of the data
+
+datas = np.array([[(elapsed_time - delay_time), bmp.temperature, bmp.pressure,
+                 accel.acceleration[0], accel.acceleration[1], accel.acceleration[2],
+                 mag.magnetic[0], mag.magnetic[1], mag.magnetic[2]]])
 
 while time.time() - start_time < delay_time + record_time:
     # Update elapsed time with every run
@@ -49,39 +57,33 @@ while time.time() - start_time < delay_time + record_time:
     # Skip everything else if we're still in the delay period
     if elapsed_time < delay_time:
         continue
-    # Otherwise, append data to dataframe
-    df = df.append({'time':(elapsed_time - delay_time),
-                    'temperature':bmp.temperature,
-                    'pressure':bmp.pressure,
-                    'acc_x':accel.acceleration[0],
-                    'acc_y':accel.acceleration[1],
-                    'acc_z':accel.acceleration[2],
-                    'mag_x':mag.magnetic[0],
-                    'mag_y':mag.magnetic[1],
-                    'mag_z':mag.magnetic[2]},
-                    ignore_index=True)
-    # start the counter
+    # Append data to the array
+    data = np.array([[(elapsed_time - delay_time), bmp.temperature, bmp.pressure,
+                 accel.acceleration[0], accel.acceleration[1], accel.acceleration[2],
+                 mag.magnetic[0], mag.magnetic[1], mag.magnetic[2]]])
+    datas = np.append(datas, data, axis=0)
+
     i += 1
-    # save a file every so often to ensure bullet-proofness depending on iterations
-    if (i % iters) == 0:
-        fo = open(f'/home/pi/RISE/data/data_temporary_{int(i / iters)}.csv')
-        df.to_csv(f'/home/pi/RISE/data/data_temporary_{int(i / iters)}.csv', index = False)
-        # FOR TESTING
-        fo.close()
 
-        print('data saved!')
+    #save the data every so often
+    if ((i + 1) % iters) == 0:
+        #TESTING
+        test_time = time.time()
+        file = open(data_dir + '/' + f'data{int(i / iters)}.csv', "w")
+        np.savetxt(data_dir + '/' + f'data{int(i / iters)}.csv', datas, delimiter=",")
+        file.close()
+        #wipe the array to keep the same save time
+        datas = np.array([[]])
+        datas = np.array([[(elapsed_time - delay_time), bmp.temperature, bmp.pressure,
+                 accel.acceleration[0], accel.acceleration[1], accel.acceleration[2],
+                 mag.magnetic[0], mag.magnetic[1], mag.magnetic[2]]])
+        #testing
+        print("--- %s seconds to save data ---" % (time.time() - test_time))
 
-# save all the data in one file, if the program ran successfully
-df.to_csv('/home/pi/RISE/data/data.csv', index = False)
+#save the last bit of data after the final loop
+file = open(data_dir + '/' + f'data{int((i / iters) + 1)}.csv', "w")
+np.savetxt(data_dir + '/' + f'data{int((i / iters) + 1)}.csv', datas, delimiter=",")
+file.close()
 
-# Delete the temporary files if the program ran successfully
-for file_name in os.listdir('/home/pi/RISE/data'):
-    if file_name.startswith('data_temporary') and file_name.endswith('.csv'):
-        os.remove(f'/home/pi/RISE/data/{file_name}')
 
-# Stop the camera recording
-#cam.stop_recording()
-
-# FOR TESTING
-print('Finished')
-print(elapsed_time)
+print('done sir')
